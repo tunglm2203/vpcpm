0d87946fc1f5858875f06aa65b562e4a5e5df392
tunglm <tunglm2203@kaist.ac.kr>
2020-10-07 17:01:52 +0900

    Add script for training with depth

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # TODO: For state
 #CUDA_VISIBLE_DEVICES=0 python train.py \
 #    --domain_name cartpole \
@@ -12,6 +11,53 @@
 #    --eval_freq 10000 --batch_size 128 --num_train_steps 200000\
 #    --save_tb
 
+#ENV_NAME='reach_target-state-v0'
+#CUDA_VISIBLE_DEVICES=0 python train_rlbench.py \
+#    --domain_name panda \
+#    --task_name $ENV_NAME \
+#    --encoder_type identity --work_dir ./logs \
+#    --action_repeat 1 --num_eval_episodes 10 \
+#    --pre_transform_image_size 100 --image_size 84 \
+#    --agent rad_sac --frame_stack 1  \
+#    --seed 23 --critic_lr 1e-3 --actor_lr 1e-3 --critic_tau 0.01\
+#    --eval_freq 10000 --batch_size 128 --num_train_steps 200000\
+#    --training_freq 1 --num_updates 1 \
+#    --save_tb --save_model
+
+#CUDA_VISIBLE_DEVICES=0 python train_rlbench.py \
+#    --domain_name panda \
+#    --task_name $ENV_NAME \
+#    --encoder_type identity --work_dir ./logs \
+#    --action_repeat 1 --num_eval_episodes 10 \
+#    --agent rad_sac --frame_stack 1  \
+#    --seed 1 \
+#    --critic_lr 1e-3 --critic_beta 0.9 --actor_lr 1e-3 --actor_beta 0.9 --critic_tau 0.01 \
+#    --alpha_lr 1e-4 --alpha_beta 0.5 \
+#    --exp 'original_alpha_lr_1e-4_beta_0.5_actorfreq_1'\
+#    --actor_update_freq 1 \
+#    --critic_target_update_freq 2 \
+#    --eval_freq 1000 --batch_size 128 --num_train_steps 100000 --init_steps 1000 \
+#    --training_freq 1 --num_updates 1 \
+#    --save_tb --save_model
+
+# Same with rlkit
+#ENV_NAME='reach_target-state-v0'
+#CUDA_VISIBLE_DEVICES=0 python train_rlbench.py \
+#    --domain_name panda \
+#    --task_name $ENV_NAME \
+#    --encoder_type identity --work_dir ./logs \
+#    --action_repeat 1 --num_eval_episodes 10 \
+#    --agent rad_sac --frame_stack 1 \
+#    --seed 1 \
+#    --exp 'rlkit-logstdmin-20-lr3e_4' \
+#    --critic_beta 0.9 --critic_lr 3e-4 --actor_beta 0.9 --actor_lr 3e-4 --critic_tau 0.001 \
+#    --alpha_lr 3e-4 --alpha_beta 0.9 \
+#    --actor_log_std_min -20 --actor_log_std_max 2 \
+#    --critic_target_update_freq 1 --actor_update_freq 1 \
+#    --eval_freq 1000 --batch_size 256 --num_train_steps 100000 --init_steps 1000 \
+#    --training_freq 500 --num_updates 50 \
+#    --save_tb --save_model
+
 # TODO:  For image
 #CUDA_VISIBLE_DEVICES=1 python train.py \
 #    --domain_name cartpole \
@@ -22,3 +68,21 @@
 #    --agent rad_sac --frame_stack 3 --data_augs crop  \
 #    --seed 23 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 200000\
 #    --save_tb
+
+ENV_NAME='reach_target-vision-v0'
+EXP_NAME='rlkit-framestack1-update50-no_aug-simple-env-depth'
+CUDA_VISIBLE_DEVICES=0 python train_rlbench.py \
+    --domain_name panda \
+    --task_name $ENV_NAME \
+    --encoder_type pixel --work_dir ./logs \
+    --action_repeat 1 --num_eval_episodes 10 \
+    --agent rad_sac --frame_stack 1 --data_augs no_aug \
+    --seed 1 \
+    --critic_beta 0.9 --critic_lr 3e-4 --actor_beta 0.9 --actor_lr 3e-4 --critic_tau 0.001 \
+    --alpha_lr 3e-4 --alpha_beta 0.9 \
+    --actor_log_std_min -20 --actor_log_std_max 2 \
+    --exp $EXP_NAME \
+    --critic_target_update_freq 1 --actor_update_freq 1 \
+    --eval_freq 1000 --batch_size 256 --num_train_steps 100000 --init_steps 1000 \
+    --training_freq 500 --num_updates 50 \
+    --save_tb --save_model --use_depth
