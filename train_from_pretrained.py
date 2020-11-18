98855ae341311d4be38a229b3003944c0f96e52b
tunglm <tunglm2203@kaist.ac.kr>
2020-11-18 14:54:48 +0900

    Change printing information, including seed

--- train_from_pretrained.py
+++ train_from_pretrained.py
@@ -304,7 +304,7 @@ def main():
         # evaluate agent periodically
 
         if step % args.eval_freq == 0:
-            print('[INFO] Experiment: ', args.exp)
+            print('[INFO] Experiment: {} - seed:{}'.format(args.exp, args.seed))
             L.log('eval/episode', episode, env_step)
             evaluate(env, agent, video, args.num_eval_episodes, L, step, env_step, args)
             if args.save_model:
